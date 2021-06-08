d7651a783e2d4047ae78d3830d11b7bff52b69e6
Ultra-Code <mega.alpha100@gmail.com>
2021-06-08 23:14:44 +0000

    Add zig to treesitter ensure_installed

--- lua/plugrc/treesitter.lua
+++ lua/plugrc/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {'cpp','c','yaml','comment','json','haskell','lua','python'}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = {'cpp','c','zig','yaml','comment','json','haskell','lua','python'}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   ignore_install = {}, -- List of parsers to ignore installing
   highlight = {
     enable = true,              -- false will disable the whole extension
