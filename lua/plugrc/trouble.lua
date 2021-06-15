c7a39eb5b3d70f216c5f5d75ef4f4346bbfa60f4
Ultra-Code <mega.alpha100@gmail.com>
2021-06-15 09:26:53 +0000

    Set troubles default mode to diagnostics mode
    Also enable auto close when there are no diagnostics

--- lua/plugrc/trouble.lua
+++ lua/plugrc/trouble.lua
@@ -4,7 +4,7 @@ require("trouble").setup
     height = 10, -- height of the trouble list when position is top or bottom
     width = 50, -- width of the list when position is left or right
     icons = true, -- use devicons for filenames
-    mode = "lsp_workspace_diagnostics", -- "lsp_workspace_diagnostics", "lsp_document_diagnostics", "quickfix", "lsp_references", "loclist"
+    mode = "lsp_document_diagnostics", -- "lsp_workspace_diagnostics", "lsp_document_diagnostics", "quickfix", "lsp_references", "loclist"
     fold_open = "", -- icon used for open folds
     fold_closed = "", -- icon used for closed folds
     action_keys = { -- key mappings for actions in the trouble list
@@ -30,7 +30,7 @@ require("trouble").setup
     },
     indent_lines = true, -- add an indent guide below the fold icons
     auto_open = false, -- automatically open the list when you have diagnostics
-    auto_close = false, -- automatically close the list when you have no diagnostics
+    auto_close = true, -- automatically close the list when you have no diagnostics
     auto_preview = true, -- automatyically preview the location of the diagnostic. <esc> to close preview and go back to last window
     auto_fold = false, -- automatically fold a file trouble list at creation
     signs = {
