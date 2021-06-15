efadd58e46043259ad0f7f29efb374d8b629fa6c
Ultra-Code <mega.alpha100@gmail.com>
2021-06-15 09:20:26 +0000

    Set some mappings based on conditonal capabilities of language servers
    This help to enable only the features supported by a particular language
    and prevents creating mappings for unsupported features, which prevents
    errors echoed to neovims message

--- lua/plugrc/lspconfig.lua
+++ lua/plugrc/lspconfig.lua
@@ -1,28 +1,69 @@
 local lsp=require('lspconfig')
+
+local on_attach = function(client,buffer)
+
+     local function set_keymap(...)
+        vim.api.nvim_buf_set_keymap(buffer, ...)
+    end
+
+    local opts = {noremap = true, silent = true}
+
+-- Set some keybinds conditional on server capabilities
+    if client.resolved_capabilities.document_formatting == true then
+        set_keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    elseif client.resolved_capabilities.document_range_formatting == true then
+        set_keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+    end
+
+     -- Set autocommands conditional on server_capabilities
+    if client.resolved_capabilities.document_highlight ==true then
+        -- lsp highlight element under cursor
+        vim.cmd[[
+        highlight link LspReferenceText WildMenu
+        highlight LspReferenceRead gui=italic guifg=#232326 guibg=#c49060 "Uses IncSearch hi-grp
+        highlight LspReferenceWrite gui=bold guifg=#232326 guibg=#e2c792 "Uses Search hi-grp
+        augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold  <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+        augroup END
+        ]]
+   end
+
+    -- client.resolved_capabilities.type_definition =false
+    if client.resolved_capabilities.type_definition == true then
+        set_keymap('n', '<leader>dt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    end
+
+end
+
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true;
 lsp.clangd.setup{
 cmd =
 { "clangd", "--query-driver=/usr/bin/g++","--clang-tidy","-j=5","--malloc-trim" },
-capabilities=capabilities;
+capabilities=capabilities,
+on_attach= on_attach,
 filetypes = { "c", "cpp"},
   --  on_init = function to handle changing offsetEncoding
 root_dir = lsp.util.root_pattern("compile_commands.json", ".git"),
 }
 
-lsp.hls.setup{capabilities=capabilities;}
+lsp.hls.setup{capabilities=capabilities, on_attach=on_attach}
 require'lspconfig'.zls.setup
 {
     cmd={"zls"},
     filetypes = {"zig","zir"},
-    capabilities=capabilities;
+    capabilities=capabilities,
+    on_attach=on_attach,
     root_dir = lsp.util.root_pattern("zls.json", ".git")
 }
 require('lspsaga').init_lsp_saga()
  cfg = {
   bind = true, -- This is mandatory, otherwise border config won't get registered.
                -- If you want to hook lspsaga or other signature handler, pls set to false
-  doc_lines = 9, -- will show two lines of comment/doc
+  doc_lines = 18,-- will show two lines of comment/doc
                  --(if there are more than two lines in doc, will be truncated);
                  -- set to 0 if you DO NOT want any API comments be shown
                  -- This setting only take effect in insert mode,
