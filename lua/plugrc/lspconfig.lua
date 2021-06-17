9c46128d23b3e3be29bd2cd2de0ad228a1165b87
Ultra-Code <mega.alpha100@gmail.com>
2021-06-17 17:02:57 +0000

    require lsp_singnature's attach function in the on_attach function

--- lua/plugrc/lspconfig.lua
+++ lua/plugrc/lspconfig.lua
@@ -1,6 +1,35 @@
 local lsp=require('lspconfig')
 
+ signature_cfg = {
+  bind = true, -- This is mandatory, otherwise border config won't get registered.
+               -- If you want to hook lspsaga or other signature handler, pls set to false
+  doc_lines = 18,-- will show two lines of comment/doc
+                 --(if there are more than two lines in doc, will be truncated);
+                 -- set to 0 if you DO NOT want any API comments be shown
+                 -- This setting only take effect in insert mode,
+    -- it does not affect signature help in normal
+                 -- mode, 10 by default
+
+  floating_window = true, -- show hint in a floating window,
+    --set to false for virtual text only mode
+  hint_enable = true, -- virtual hint enable
+  hint_prefix = "🐼 ",  -- Panda for parameter
+  hint_scheme = "String",
+  use_lspsaga = false,  -- set to true if you want to use lspsaga popup
+  hi_parameter = "Search", -- how your parameter will be highlight
+  max_height = 18, -- max height of signature floating_window,
+    --if content is more than max_height, you can scroll down
+                   -- to view the hiding contents
+  max_width = 120, -- max_width of signature floating_window,
+    --line will be wrapped if exceed max_width
+  handler_opts = {
+    border = "single"   -- double, single, shadow, none
+  },
+}
+
 local on_attach = function(client,buffer)
+
+    require'lsp_signature'.on_attach(signature_cfg)
 
      local function set_keymap(...)
         vim.api.nvim_buf_set_keymap(buffer, ...)
@@ -60,31 +89,3 @@ require'lspconfig'.zls.setup
     root_dir = lsp.util.root_pattern("zls.json", ".git")
 }
 require('lspsaga').init_lsp_saga()
- cfg = {
-  bind = true, -- This is mandatory, otherwise border config won't get registered.
-               -- If you want to hook lspsaga or other signature handler, pls set to false
-  doc_lines = 18,-- will show two lines of comment/doc
-                 --(if there are more than two lines in doc, will be truncated);
-                 -- set to 0 if you DO NOT want any API comments be shown
-                 -- This setting only take effect in insert mode,
-    -- it does not affect signature help in normal
-                 -- mode, 10 by default
-
-  floating_window = true, -- show hint in a floating window,
-    --set to false for virtual text only mode
-  hint_enable = true, -- virtual hint enable
-  hint_prefix = "🐼 ",  -- Panda for parameter
-  hint_scheme = "String",
-  use_lspsaga = false,  -- set to true if you want to use lspsaga popup
-  hi_parameter = "Search", -- how your parameter will be highlight
-  max_height = 18, -- max height of signature floating_window,
-    --if content is more than max_height, you can scroll down
-                   -- to view the hiding contents
-  max_width = 120, -- max_width of signature floating_window,
-    --line will be wrapped if exceed max_width
-  handler_opts = {
-    border = "single"   -- double, single, shadow, none
-  },
-}
-
-require'lsp_signature'.on_attach(cfg)
