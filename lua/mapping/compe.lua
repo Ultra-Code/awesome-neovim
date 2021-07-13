8bda41f76bfa0f78e4c81649fc6eff33935e6452
Ultra-Code <mega.alpha100@gmail.com>
2021-07-13 02:58:20 +0000

    use luasnip for tab autocompletion

--- lua/mapping/compe.lua
+++ lua/mapping/compe.lua
@@ -35,8 +35,8 @@ end
 _G.tab_complete = function()
   if vim.fn.pumvisible() == 1 then
     return t "<C-n>"
-  elseif vim.fn.call("vsnip#available", {1}) == 1 then
-    return t "<Plug>(vsnip-expand-or-jump)"
+  elseif require'luasnip'.expand_or_jumpable() then
+    return t "<Plug>luasnip-expand-or-jump"
   elseif check_back_space() then
     return t "<Tab>"
   else
@@ -46,8 +46,8 @@ end
 _G.s_tab_complete = function()
   if vim.fn.pumvisible() == 1 then
     return t "<C-p>"
-  elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
-    return t "<Plug>(vsnip-jump-prev)"
+  elseif require'luasnip'.jumpable(-1) then
+    return t "<Plug>luasnip-jump-prev"
   else
     -- If <S-Tab> is not working in your terminal, change it to <C-h>
     return t "<S-Tab>"
