1c2deacc5aa2be546e6cbd08740aa8bd1390099f
Ultra-Code <mega.alpha100@gmail.com>
2021-07-14 17:35:18 +0000

    Use efm for linting and formating
    Add lua-formatter with lua-format
    
    Add c and cpp linter with cppcheck

--- lua/plugrc/lspconfig/settings.lua
+++ lua/plugrc/lspconfig/settings.lua
@@ -17,17 +17,49 @@ settings.lua_settings = {
             library = vim.api.nvim_get_runtime_file('', true)
         }
     }
-};
+}
+
+local linters = {}
+
+linters.cppcheck = {
+    cpp = {
+        lintCommand = 'cppcheck --enable=warning,style,performance,portability,information,missingInclude --inconclusive -j 4 --template=gcc --language=c++ ${INPUT}',
+        lintStdin = true,
+        lintFormats = {'%f:%l:%c: %m'},
+        lintIgnoreExitCode = true
+    },
+    c = {
+        lintCommand = 'cppcheck --enable=warning,style,performance,portability,information,missingInclude --inconclusive -j 4 --template=gcc --language=c ${INPUT}',
+        lintStdin = true,
+        lintFormats = {'%f:%l:%c: %m'},
+        lintIgnoreExitCode = true
+    }
+}
+
+local formatters = {}
+
+formatters.lua_format = {formatCommand = "lua-format -i", formatStdin = true}
 
 settings.efm_settings = {
-    init_options = {documentFormatting = true},
+    log_level = 0,
+    log_file = '/tmp/efm.log',
+    init_options = {
+        documentFormatting = true,
+        codeAction = true,
+        hover = true,
+        documentSymbol = true,
+        completion = false
+    },
+    filetypes = {"lua", "cpp", "c"},
     settings = {
         rootMarkers = {".git/"},
         languages = {
-            lua = {{formatCommand = "lua-format -i", formatStdin = true}}
+            lua = {formatters.lua_format},
+            cpp = {linters.cppcheck.cpp},
+            c = {linters.cppcheck.c}
         }
     }
-};
+}
 
 settings.clangd_setting = {
     cmd = {
@@ -35,6 +67,6 @@ settings.clangd_setting = {
         "--malloc-trim"
     },
     filetypes = {"c", "cpp"} -- we don't want objective-c and objective-cpp!
-};
+}
 
 return settings
