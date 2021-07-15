cbd2f3037eb41361b29cd7c93f7e6f7550980923
Ultra-Code <mega.alpha100@gmail.com>
2021-07-15 23:55:32 +0000

    cleanup(config): remove all unused plugins configurations

--- lua/plugrc/init.lua
+++ lua/plugrc/init.lua
@@ -2,21 +2,12 @@
 --==             PLUGINS INIT                             ==
 --==========================================================
 --
-local cmd=vim.cmd
-cmd[["source  $XDG_CONFIG_HOME/nvim/lua/plugrc/coc.vim]]
-cmd[["source  $XDG_CONFIG_HOME/nvim/lua/plugrc/markdown_preview.vim]]
-cmd[["source  $XDG_CONFIG_HOME/nvim/lua/plugrc/syntastic.vim]]
-cmd[[source  $XDG_CONFIG_HOME/nvim/lua/plugrc/neoformat.vim]]
-cmd[["source  $XDG_CONFIG_HOME/nvim/lua/plugrc/nerdtree_git.vim]]
-cmd[["source  $XDG_CONFIG_HOME/nvim/lua/plugrc/airline.vim]]
-cmd[["source  $XDG_CONFIG_HOME/nvim/lua/plugrc/fzf.vim]]
 require('plugrc/vista')
 require('plugrc/tree')
 require('plugrc/lspconfig')
-require('gitsigns').setup()
+require('plugrc/gitsigns')
 require('plugrc/compe')
 require('nvim-autopairs').setup()
-require('plugrc/neomake')
 require('telescope').setup()
 require('plugrc/trouble')
 require('plugrc/whichkey')
--- lua/plugrc/multiple_cursor.vim
+++
@@ -1,2 +0,0 @@
-let g:multi_cursor_use_default_mapping=0
-
