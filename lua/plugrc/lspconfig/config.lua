4f16b81f667a715f2b414e268c45d5ff0094cabe
Ultra-Code <mega.alpha100@gmail.com>
2021-07-15 18:40:11 +0000

    Builtin lsp configurations
    Disable virtual text and prefer floating window
    
    Add lsp completion kinds
    
    Modify gutter with user defined icons

---
+++ lua/plugrc/lspconfig/config.lua
@@ -0,0 +1,55 @@
+local M = {}
+
+M.icons = {
+    Class = " ",
+    Color = " ",
+    Constant = " ",
+    Constructor = " ",
+    Enum = "了 ",
+    EnumMember = " ",
+    Field = " ",
+    File = " ",
+    Folder = " ",
+    Function = " ",
+    Interface = "ﰮ ",
+    Keyword = " ",
+    Method = "ƒ ",
+    Module = " ",
+    Property = " ",
+    Snippet = "﬌ ",
+    Struct = " ",
+    Text = " ",
+    Unit = " ",
+    Value = " ",
+    Variable = " "
+}
+
+function M.completion_kinds()
+    local kinds = vim.lsp.protocol.CompletionItemKind
+    for i, kind in ipairs(kinds) do kinds[i] = M.icons[kind] or kind end
+end
+
+function M.sign_column_diagnostic_symbols()
+    local signs = {
+        Error = " ",
+        Warning = " ",
+        Hint = " ",
+        Information = " "
+    }
+    for type, icon in pairs(signs) do
+        local hl = "LspDiagnosticsSign" .. type
+        vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
+    end
+end
+
+function M.disable_virtual_text()
+    vim.lsp.handlers["textDocument/publishDiagnostics"] =
+        vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+            virtual_text = false,
+            signs = true,
+            update_in_insert = true,
+            underline = true
+        })
+end
+
+return M
