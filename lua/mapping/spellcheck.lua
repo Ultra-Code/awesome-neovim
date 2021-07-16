e5fb4733f0946cec98502191a24ee430763d9be6
Ultra-Code <mega.alpha100@gmail.com>
2021-07-16 04:47:10 +0000

    refactor(vim->lua): change mappings to use vim lua api

--- lua/mapping/spellcheck.lua
+++ lua/mapping/spellcheck.lua
@@ -1,14 +1,24 @@
-" === Spell checking === "
-" For help on spell checking :help spell
+require('utils')
+-- === Spell checking === "
+-- For help on spell checking :help spell
 
-" Pressing ,ts will toggle and untoggle spell checking
-map <leader>ts :setlocal spell!<cr> "toggle to alternate configuration of spellcheck
+-- Pressing ,ts will toggle and untoggle spell checking
+-- toggle to alternate configuration of spellcheck
+local opt = {noremap = false}
+map('', '<leader>ts', ':setlocal spell!<cr>', opt)
 
-" Shortcuts using <leader>
-map <leader>sn ]s   "next spellcheck iteam
-map <leader>sp [s   "previous spellcheck iteam
-map <leader>sg zg   "mark word as good in local spellfile :NOTE checkout zG for internal spellfile
-map <leader>sw zw   "mark word as wrong in local spellfile :NOTE checkout zW for internal spellfile
-map <leader>usg zug "undo marking of word as good
-map <leader>usw zuw "undo marking of word as wrong
-map <leader>sc z=   "check for suggestions for the underlined word
+-- Shortcuts using <leader>
+-- next spellcheck iteam
+map('', '<leader>sn', ']s', opt)
+-- previous spellcheck iteam
+map('', '<leader>sp', 's[', opt)
+-- mark word as good in local spellfile :NOTE checkout zG for internal spellfile
+map('', '<leader>sg', 'gz', opt)
+-- mark word as wrong in local spellfile :NOTE checkout zW for internal spellfile
+map('', '<leader>sw', 'wz', opt)
+-- undo marking of word as good
+map('', '<leader>usg', 'ugz', opt)
+-- undo marking of word as wrong
+map('', '<leader>usw', 'uwz', opt)
+-- check for suggestions for the underlined word
+map('', '<leader>sc', '=z', opt)
--- lua/mapping/tab_buffer.lua
+++ lua/mapping/tab_buffer.lua
@@ -1,53 +1,46 @@
-" === Moving around, tabs, windows and buffers === "
+require('utils')
+-- === Moving around, tabs, windows and buffers === --
 
-"To use `ALT+{h,j,k,l}` to navigate windows from any mode: >
-:tnoremap <A-h> <C-\><C-N><C-w>h
-:tnoremap <A-j> <C-\><C-N><C-w>j
-:tnoremap <A-k> <C-\><C-N><C-w>k
-:tnoremap <A-l> <C-\><C-N><C-w>l
-:inoremap <A-h> <C-\><C-N><C-w>h
-:inoremap <A-j> <C-\><C-N><C-w>j
-:inoremap <A-k> <C-\><C-N><C-w>k
-:inoremap <A-l> <C-\><C-N><C-w>l
-:nnoremap <A-h> <C-w>h
-:nnoremap <A-j> <C-w>j
-:nnoremap <A-k> <C-w>k
-:nnoremap <A-l> <C-w>l
+-- To use `ALT+{h,j,k,l}` to navigate windows from any mode: >
+map('t', '<A-h>', [[<C-\><C-N><C-w>h]])
+map('t', '<A-j>', [[<C-\><C-N><C-w>j]])
+map('t', '<A-k>', [[<C-\><C-N><C-w>k]])
+map('t', '<A-l>', [[<C-\><C-N><C-w>l]])
+map('i', '<A-h>', [[<C-\><C-N><C-w>h]])
+map('i', '<A-j>', [[<C-\><C-N><C-w>j]])
+map('i', '<A-k>', [[<C-\><C-N><C-w>k]])
+map('i', '<A-l>', [[<C-\><C-N><C-w>l]])
+map('n', '<A-h>', '<C-w>h')
+map('n', '<A-j>', '<C-w>j')
+map('n', '<A-k>', '<C-w>k')
+map('n', '<A-l>', '<C-w>l')
 
-" Close the current buffer
-map <leader>bd :bdelete<cr>
+-- Close the current buffer
+local opt = {noremap = false}
+map('', '<leader>bd', ':bdelete<cr>', opt)
 
-" Close all the buffers
-map <leader>ba :bufdo bd<cr>
+-- Close all the buffers
+map('', '<leader>ba', ':bufdo bd<cr>', opt)
 
-map <leader>bn :bnext<cr>
-map <leader>bp :bprevious<cr>
-map <leader>bf :bfirst<cr>
-map <leader>bl :blast<cr>
+map('', '<leader>bn', ':bnext<cr>', opt)
+map('', '<leader>bp', ':bprevious<cr>', opt)
+map('', '<leader>bf', ':bfirst<cr>', opt)
+map('', '<leader>bl', ':blast<cr>', opt)
 
+-- Useful mappings for managing tabs
+map('', '<leader>tnw', ':tabnew<cr>', opt)
+map('', '<leader>to', ':tabonly<cr>', opt)
+map('', '<leader>tc', ':tabclose<cr>', opt)
+map('', '<leader>tm', ':tabmove', opt)
+map('', '<leader>tn', ':tabnext<cr>', opt)
+map('', '<leader>tp', ':tabprevious<cr>', opt)
+map('', '<leader>tf', ':tabfirst<cr>', opt)
+-- pass a positive or negative argument to the command to move by that step--
+map('', '<leader>t', ':tabnext', opt)
 
-" Useful mappings for managing tabs
-map <leader>tnw :tabnew<cr>
-map <leader>to :tabonly<cr>
-map <leader>tc :tabclose<cr>
-map <leader>tm :tabmove
-map <leader>tn :tabnext<cr>
-map <leader>tp :tabprevious<cr>
-map <leader>tf :tabfirst<cr>
-"pass a positive or negative argument to the command to move by that step"
-map <leader>t :tabnext
+-- Opens a new tab with the current buffer's path
+-- Super useful when editing files in the same directory
+map('', '<leader>te', ':tabedit <c-r>=expand("%:p:h")<cr><cr>', opt)
 
-" Let 'tl' toggle between this and the last accessed tab
-let g:lasttab = 1
-nmap <Leader>tl :exe "tabn ".g:lasttab<CR>
-au TabLeave * let g:lasttab = tabpagenr()
-
-
-" Opens a new tab with the current buffer's path
-" Super useful when editing files in the same directory
-map <leader>te :tabedit <c-r>=expand("%:p:h")<cr><cr>
-
-" Switch CWD to the directory of the open buffer
-map <leader>cd :cd %:p:h<cr>:pwd<cr>
-
-"nmap <C-[> :pop<cr>
+-- Switch CWD to the directory of the open buffer
+map('', '<leader>cd', ':cd %:p:h<cr>:pwd<cr>', opt)
