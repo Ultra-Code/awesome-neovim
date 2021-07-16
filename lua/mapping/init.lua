7e93579d7e307035e4705b274ef48447edb77798
Ultra-Code <mega.alpha100@gmail.com>
2021-07-16 04:50:03 +0000

    load convert vim mapping files at init point

--- lua/mapping/init.lua
+++ lua/mapping/init.lua
@@ -1,17 +1,7 @@
---Source the various mappings
-local cmd= vim.cmd
-cmd[["source  $XDG_CONFIG_HOME/nvim/lua/mapping/coc_map.vim]]
-cmd[["source   $XDG_CONFIG_HOME/nvim/lua/mapping/fzf_map.vim]]
-cmd[["source   $XDG_CONFIG_HOME/nvim/lua/mapping/nerdtree_map.vim]]
-cmd[[source   $XDG_CONFIG_HOME/nvim/lua/mapping/copy_paste_map.vim]]
-cmd[[source   $XDG_CONFIG_HOME/nvim/lua/mapping/visual_fnd_replace_map.vim]]
-cmd[[source   $XDG_CONFIG_HOME/nvim/lua/mapping/spellcheck_map.vim]]
-cmd[[source   $XDG_CONFIG_HOME/nvim/lua/mapping/tab_buffer_map.vim]]
-cmd[["source   $XDG_CONFIG_HOME/nvim/lua/mapping/syntastic_map.vim]]
-cmd[["source   $XDG_CONFIG_HOME/nvim/lua/mapping/markdown_preview_map.vim]]
-cmd[[source   $XDG_CONFIG_HOME/nvim/lua/mapping/highlighting_map.vim]]
-cmd[[source   $XDG_CONFIG_HOME/nvim/lua/mapping/folding_map.vim]]
-cmd[[source   $XDG_CONFIG_HOME/nvim/lua/mapping/terminal_map.vim]]
+require('mapping/spellcheck')
+require('mapping/tab_buffer')
+require('mapping/highlighting')
+require('mapping/terminal')
 require('mapping/compe')
 require('mapping/lspconfig')
 require('mapping/autopairs')
@@ -19,3 +9,4 @@ require('mapping/tree')
 require('mapping/telescope')
 require('mapping/trouble')
 require('mapping/vista')
+require('mapping/tags')
