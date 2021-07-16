e6fc4e4816612a9e89916d49582479aa7b1afa64
Ultra-Code <mega.alpha100@gmail.com>
2021-07-16 04:27:36 +0000

    feature: Add termcode and dump utility functions

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,12 +1,24 @@
 function _G.map(mode, lhs, rhs, opts)
-  local options = {noremap = true}
-  if opts then options = vim.tbl_extend('force', options, opts) end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+    local options = {noremap = true}
+    if opts then options = vim.tbl_extend('force', options, opts) end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
 
 function _G.opt(scope, key, value)
-  scopes[scope][key] = value
-  if scope ~= 'o' then scopes['o'][key] = value end
+    scopes[scope][key] = value
+    if scope ~= 'o' then scopes['o'][key] = value end
+end
+
+function _G.dump(...)
+    local objects = vim.tbl_map(vim.inspect, {...})
+    print(unpack(objects))
+    return ...
+end
+
+-- escape termcode and keycode in lua for use in mapping expressions
+function _G.t(str)
+    -- Adjust boolean arguments as needed
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
