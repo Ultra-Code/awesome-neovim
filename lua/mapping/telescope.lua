420cfc3a4dc7ac72971e5b8150bfca400b35c49b
Ultra-Code <mega.alpha100@gmail.com>
2021-07-21 01:13:05 +0000

    restructure : make telescope optional

--- lua/mapping/telescope.lua
+++ lua/mapping/telescope.lua
@@ -1,7 +1,7 @@
 -- Using lua functions
-local map=vim.api.nvim_set_keymap
-local options={noremap=true,silent=true}
-map('n','<leader>ff',[[<cmd>lua require('telescope.builtin').find_files()<cr>]] ,options)
-map('n','<leader>fg',[[<cmd>lua require('telescope.builtin').live_grep()<cr>]] ,options)
-map('n','<leader>fb',[[<cmd>lua require('telescope.builtin').buffers()<cr>]] ,options)
-map('n','<leader>fh',[[<cmd>lua require('telescope.builtin').help_tags()<cr>]],options)
+require('utils')
+local opt={silent=true}
+map('n','<leader>ff',[[<cmd>packadd popup.nvim<cr>|<cmd>packadd telescope.nvim<cr>|<cmd>lua require('telescope.builtin').find_files()<cr>]] ,opt)
+map('n','<leader>fg',[[<cmd>packadd popup.nvim<cr>|<cmd>packadd telescope.nvim<cr>|<cmd>lua require('telescope.builtin').live_grep()<cr>]] ,opt)
+map('n','<leader>fb',[[<cmd>packadd popup.nvim<cr>|<cmd>packadd telescope.nvim<cr>|<cmd>lua require('telescope.builtin').buffers()<cr>]] ,opt)
+map('n','<leader>fh',[[<cmd>packadd popup.nvim<cr>|<cmd>packadd telescope.nvim<cr>|<cmd>lua require('telescope.builtin').help_tags()<cr>]],opt)
