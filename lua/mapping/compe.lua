110dba7823774ed256035ff47afaae045fe57b24
Ultra-Code <mega.alpha100@gmail.com>
2021-07-21 06:29:17 +0000

    refactor: Use equivalent functions in utils

--- lua/mapping/compe.lua
+++ lua/mapping/compe.lua
@@ -1,24 +1,4 @@
-local function map(mode, lhs, rhs, opts)
-local options= {noremap=true,silent=true,expr=true}
-  if opts then options = vim.tbl_extend('force', options, opts) end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
---local fn=vim.fn
---map('i','<C-Space>','compe#complete()')
---map('i','<CR>',  'compe#confirm(<CR>)')
--- Mappings for auto import
-vim.cmd[[
-inoremap <silent><expr> <C-Space> compe#complete()
-inoremap <silent><expr> <CR>      compe#confirm('<CR>')
-inoremap <silent><expr> <C-e>     compe#close('<C-e>')
-inoremap <silent><expr> <C-f>     compe#scroll({ 'delta': +4 })
-inoremap <silent><expr> <C-d>     compe#scroll({ 'delta': -4 })
-]]
-
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
+require('utils')
 
 local check_back_space = function()
     local col = vim.fn.col('.') - 1
@@ -33,28 +13,28 @@ end
 --- move to prev/next item in completion menuone
 --- jump to prev/next snippet's placeholder
 _G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-n>"
-  elseif require'luasnip'.expand_or_jumpable() then
-    return t "<Plug>luasnip-expand-or-jump"
-  elseif check_back_space() then
-    return t "<Tab>"
-  else
-    return vim.fn['compe#complete']()
-  end
+    if vim.fn.pumvisible() == 1 then
+        return t "<C-n>"
+    elseif require'luasnip'.expand_or_jumpable() then
+        return t "<Plug>luasnip-expand-or-jump"
+    elseif check_back_space() then
+        return t "<Tab>"
+    else
+        return vim.fn['compe#complete']()
+    end
 end
 _G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-p>"
-  elseif require'luasnip'.jumpable(-1) then
-    return t "<Plug>luasnip-jump-prev"
-  else
-    -- If <S-Tab> is not working in your terminal, change it to <C-h>
-    return t "<S-Tab>"
-  end
+    if vim.fn.pumvisible() == 1 then
+        return t "<C-p>"
+    elseif require'luasnip'.jumpable(-1) then
+        return t "<Plug>luasnip-jump-prev"
+    else
+        -- If <S-Tab> is not working in your terminal, change it to <C-h>
+        return t "<S-Tab>"
+    end
 end
-
-vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+local opt = {noremap = false ,expr = true}
+map("i", "<Tab>", "v:lua.tab_complete()", opt)
+map("s", "<Tab>", "v:lua.tab_complete()", opt)
+map("i", "<S-Tab>", "v:lua.s_tab_complete()", opt)
+map("s", "<S-Tab>", "v:lua.s_tab_complete()", opt)
