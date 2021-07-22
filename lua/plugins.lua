e43d608cfbdc632e8a3ea6d9169329222e6bc11d
Ultra-Code <mega.alpha100@gmail.com>
2021-07-22 01:19:39 +0000

    Glow is lazy loaded by default for md so making it a start package

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -48,7 +48,7 @@ require 'paq-nvim' {
 
 -- === Preview Plugin ===
 --A markdown previewer prebuild binary
-{'npxbr/glow.nvim',branch="main",run = ":GlowInstall",opt=true};
+{'npxbr/glow.nvim',branch="main",run = ":GlowInstall"};
 
 
 --Vim Wiki
