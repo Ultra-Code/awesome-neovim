dd1398fb085e250742c4e08bbcf2eabe19b175d8
Ultra-Code <mega.alpha100@gmail.com>
2021-07-23 23:29:40 +0000

    Add code for showing source of diagnostics
    NOTE:because this code also disables virtual text the inialt code isn't called anymore

--- lua/plugrc/lspconfig/config.lua
+++ lua/plugrc/lspconfig/config.lua
@@ -31,7 +31,7 @@ function M.completion_kinds()
 end
 
 function M.sign_column_diagnostic_symbols()
---    --  --   ﯧ   --    ﯦ
+    --    --  --   ﯧ   --    ﯦ
     local signs = {
         Error = " ",
         Warning = " ",
@@ -48,10 +48,38 @@ function M.disable_virtual_text()
     vim.lsp.handlers["textDocument/publishDiagnostics"] =
         vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
             virtual_text = false,
+            update_in_insert = false,
             signs = true,
-            update_in_insert = true,
             underline = true
         })
+end
+function M.display_diagnostics_sources()
+    vim.lsp.handlers["textDocument/publishDiagnostics"] =
+        function(_, _, params, client_id, _)
+            local config = { -- your config
+                virtual_text = false,
+                update_in_insert = false,
+                signs = true,
+                underline = true
+            }
+            local uri = params.uri
+            local bufnr = vim.uri_to_bufnr(uri)
+
+            if not bufnr then return end
+
+            local diagnostics = params.diagnostics
+
+            for i, v in ipairs(diagnostics) do
+                diagnostics[i].message =
+                    string.format("%s: %s", v.source, v.message)
+            end
+
+            vim.lsp.diagnostic.save(diagnostics, bufnr, client_id)
+
+            if not vim.api.nvim_buf_is_loaded(bufnr) then return end
+
+            vim.lsp.diagnostic.display(diagnostics, bufnr, client_id, config)
+        end
 end
 
 return M
