3e7bbb9e08bdc0b301937b9ad2dea328ebc24e9c
Ultra-Code <mega.alpha100@gmail.com>
2021-10-04 13:32:51 +0000

    Update to the new nvim-cmp auto completion plugin from nvim-compe

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,90 +8,88 @@
 --/home/$USER/.local/share/nvim/plugged
 ---Avoid using standard Vim directory names like 'plugin'
 
-require 'paq-nvim' {
+require("paq-nvim")({
 
-{'savq/paq-nvim'};
--- === Editor Plugins === --
---A commenter for different Language
-{'b3nj5m1n/kommentary',opt=true};
+    { "savq/paq-nvim" },
+    -- === Editor Plugins === --
+    --A commenter for different Language
+    { "b3nj5m1n/kommentary", opt = true },
 
--- === Themes and Colour Scheme ===
-{'navarasu/onedark.nvim'};
+    -- === Themes and Colour Scheme ===
+    { "navarasu/onedark.nvim" },
 
--- === Git Plugin ===
+    -- === Git Plugin ===
 
---A vim plugin which shows git diff in the gutter (sign column).
-{'lewis6991/gitsigns.nvim'};
+    --A vim plugin which shows git diff in the gutter (sign column).
+    { "lewis6991/gitsigns.nvim" },
 
--- === Status Line ===
+    -- === Status Line ===
 
---Status bar plugin
--- A blazing fast and easy to configure neovim statusline
-{'hoob3rt/lualine.nvim'};
--- Tabline
--- A snazzy buffer line (with minimal tab integration) for Neovim
-{'akinsho/nvim-bufferline.lua'};
+    --Status bar plugin
+    -- A blazing fast and easy to configure neovim statusline
+    { "hoob3rt/lualine.nvim" },
+    -- Tabline
+    -- A snazzy buffer line (with minimal tab integration) for Neovim
+    { "akinsho/nvim-bufferline.lua" },
 
--- === Fuzzy Finder ===
--- a highly extendable fuzzy finder over lists
--- for Find, Filter, Preview, Pick
-{'nvim-lua/popup.nvim',opt=true};
-{'nvim-lua/plenary.nvim'};
-{'nvim-telescope/telescope.nvim',opt=true};
+    -- === Fuzzy Finder ===
+    -- a highly extendable fuzzy finder over lists
+    -- for Find, Filter, Preview, Pick
+    { "nvim-lua/popup.nvim", opt = true },
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-telescope/telescope.nvim", opt = true },
 
--- === Syntax Plugins ===
---Nvim Treesitter configurations and abstraction layer
-{'nvim-treesitter/nvim-treesitter', run=':TSUpdate'};
+    -- === Syntax Plugins ===
+    --Nvim Treesitter configurations and abstraction layer
+    { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" },
 
--- === Preview Plugin ===
---A markdown previewer prebuild binary
-{'npxbr/glow.nvim',branch="main",run = ":GlowInstall"};
+    -- === Preview Plugin ===
+    --A markdown previewer prebuild binary
+    { "npxbr/glow.nvim", branch = "main", run = ":GlowInstall" },
 
---Vim Wiki
-{'vimwiki/vimwiki',branch="dev",opt=true};
+    --Vim Wiki
+    { "vimwiki/vimwiki", branch = "dev", opt = true },
 
--- === Language Specific Plugins ===
+    -- === Language Specific Plugins ===
 
+    -- == LSP Client ===
+    --Nvim LSP client
+    { "neovim/nvim-lspconfig" },
+    --A light-weight lsp plugin
+    { "tami5/lspsaga.nvim" },
+    --Show function signature when you type
+    { "ray-x/lsp_signature.nvim" },
+    -- A pretty diagnostics, references, telescope
+    -- results, quickfix and location
+    -- list to help you solve all the trouble your code is causing
+    { "folke/trouble.nvim", opt = true },
 
--- == LSP Client ===
---Nvim LSP client
-{'neovim/nvim-lspconfig'};
---A light-weight lsp plugin
-{'tami5/lspsaga.nvim'};
---Show function signature when you type
-{'ray-x/lsp_signature.nvim'};
--- A pretty diagnostics, references, telescope
--- results, quickfix and location
--- list to help you solve all the trouble your code is causing
-{'folke/trouble.nvim',opt=true};
+    --displays a popup with possible key bindings
+    --of the command you started typing
+    { "folke/which-key.nvim" },
 
---displays a popup with possible key bindings
---of the command you started typing
-{"folke/which-key.nvim"};
+    -- == Auto Completions Engine ==
+    "hrsh7th/nvim-cmp", -- Autocompletion plugin
+    "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
 
--- == Auto Completions Engine ==
-{'hrsh7th/nvim-compe'};
+    -- Auto pairs
+    { "windwp/nvim-autopairs" },
 
--- Auto pairs
-{'windwp/nvim-autopairs'};
+    -- === Code Snippets Plugins ===
+    { "L3MON4D3/LuaSnip" },
 
--- === Code Snippets Plugins ===
-{"L3MON4D3/LuaSnip"};
+    -- === Tag Bar Plugin ===
 
--- === Tag Bar Plugin ===
+    --View and search LSP symbols, tags in Vim/NeoVim.--
+    { "liuchengxu/vista.vim", opt = true },
 
---View and search LSP symbols, tags in Vim/NeoVim.--
-{'liuchengxu/vista.vim',opt=true};
+    -- === UI === --
+    -- A Neovim File explorer
+    { "kyazdani42/nvim-tree.lua" },
 
--- === UI === --
--- A Neovim File explorer
-{'kyazdani42/nvim-tree.lua'};
+    -- == Icons == --
 
--- == Icons == --
-
--- Glyphs and Icons for neovim
---Plug 'ryanoasis/vim-devicons'
-{'kyazdani42/nvim-web-devicons'};
-
--- Initialize plugin system
-}
+    -- Glyphs and Icons for neovim
+    --Plug 'ryanoasis/vim-devicons'
+    { "kyazdani42/nvim-web-devicons" },
+})
