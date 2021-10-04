2440bbd367dc1f0d8a229ac87d85b6542d23b94c
Ultra-Code <mega.alpha100@gmail.com>
2021-10-04 15:56:33 +0000

    configure nvim-cmp

--- lua/plugrc/compe.lua
+++ lua/plugrc/compe.lua
@@ -1,30 +1,74 @@
-require'compe'.setup {
-  enabled = true;
-  autocomplete = true;
-  debug = false;
-  min_length = 1;
-  preselect = 'enable';
-  throttle_time = 80;
-  source_timeout = 200;
-  incomplete_delay = 400;
-  max_abbr_width = 100;
-  max_kind_width = 100;
-  max_menu_width = 100;
-  documentation = true;
+-- Setup nvim-cmp.
+local cmp = require("cmp")
+local luasnip = require("luasnip")
 
-  source = {
-    path = true;
-    buffer = true;
-    calc = false;
-    luasnip = true;
-    ultisnips = false;
-    spell = false;
-    nvim_lua = true;
-    tags = false;
-    treesitter = false;
-    nvim_lsp = true;
-    vsnip = false;
-    emoji = true;
-  };
-}
-vim.cmd[[highlight link CompeDocumentation NormalFloat]]
+local has_words_before = function()
+    if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+        return false
+    end
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0
+        and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+                :sub(col, col)
+                :match("%s")
+            == nil
+end
+
+local t = function(key)
+    vim.fn.feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), "n")
+end
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            -- For `luasnip` user.
+            require("luasnip").lsp_expand(args.body)
+        end,
+    },
+    mapping = {
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if vim.fn.pumvisible() == 1 then
+                t "<C-n>"
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+            end
+        end, {
+            "i",
+            "s",
+        }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if vim.fn.pumvisible() == 1 then
+                t "<C-p>"
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, {
+            "i",
+            "s",
+        }),
+    },
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+        { name = "luasnip" },
+        { name = "path" },
+        { name = "nvim_lua" },
+        { name = "emoji" },
+        { name = "spell" },
+    },
+})
+--vim.cmd[[highlight link CompeDocumentation NormalFloat]]
