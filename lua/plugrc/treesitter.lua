603cf259e0441d72dd246c1beb3649714bf425b0
Ultra-Code <mega.alpha100@gmail.com>
2021-10-05 17:02:31 +0000

    Add treesitter configuration for norg

--- lua/plugrc/treesitter.lua
+++ lua/plugrc/treesitter.lua
@@ -1,16 +1,39 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = {'cpp','zig','nix','comment','json','haskell','lua','vue','typescript','scss','html'},
-    -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  ignore_install = {}, -- List of parsers to ignore installing
-  highlight = {
-    enable = true,              -- false will disable the whole extension
-    disable = {},  -- list of language that will be disabled
-  },
-  incremental_selection = {
-      enable = true
-  },
-  indent = {
-    enable = true
-  },
-  autopairs={enable = true}
+local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
+
+parser_configs.norg = {
+    install_info = {
+        url = "https://github.com/nvim-neorg/tree-sitter-norg",
+        files = { "src/parser.c", "src/scanner.cc" },
+        branch = "main",
+    },
 }
+
+require("nvim-treesitter.configs").setup({
+    ensure_installed = {
+        "cpp",
+        "zig",
+        "nix",
+        "comment",
+        "json",
+        "haskell",
+        "lua",
+        "vue",
+        "typescript",
+        "scss",
+        "html",
+        "norg"
+    },
+    -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ignore_install = {}, -- List of parsers to ignore installing
+    highlight = {
+        enable = true, -- false will disable the whole extension
+        disable = {}, -- list of language that will be disabled
+    },
+    incremental_selection = {
+        enable = true,
+    },
+    indent = {
+        enable = true,
+    },
+    autopairs = { enable = true },
+})
