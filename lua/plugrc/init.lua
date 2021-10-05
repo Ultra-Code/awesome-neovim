967ad8b14f5c8a8b9efef24bc7517fb8dba42ce6
Ultra-Code <mega.alpha100@gmail.com>
2021-10-05 17:01:25 +0000

    Add initialization for neorg

--- lua/plugrc/init.lua
+++ lua/plugrc/init.lua
@@ -2,9 +2,10 @@
 --==             PLUGINS INIT                             ==
 --==========================================================
 --
-require('plugrc/lspconfig')
-require('plugrc/gitsigns')
-require('plugrc/compe')
-require('plugrc/autopairs')
-require('plugrc/whichkey')
-require('plugrc/tree')
+require('plugrc.lspconfig')
+require('plugrc.gitsigns')
+require('plugrc.compe')
+require('plugrc.autopairs')
+require('plugrc.whichkey')
+require('plugrc.tree')
+require('plugrc.neorg')
