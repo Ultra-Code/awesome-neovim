d0efcc13cd493eb1623b8758c8b668bae7da9f27
Ultra-Code <mega.alpha100@gmail.com>
2021-10-20 04:31:48 +0000

    Remove cppcheck from settings since it has been integrated into null-ls

--- lua/plugrc/lspconfig/settings.lua
+++ lua/plugrc/lspconfig/settings.lua
@@ -31,43 +31,6 @@ settings.lua_settings = {
     },
 }
 
-local null_ls = require("null-ls")
-local helpers = require("null-ls.helpers")
-
-settings.cppcheck = {
-    method = null_ls.methods.DIAGNOSTICS,
-    filetypes = { "cpp", "c" },
-    -- null_ls.generator creates an async source
-    -- that spawns the command with the given arguments and options
-    generator = null_ls.generator({
-        command = "cppcheck",
-        args = {
-            "--enable=warning,style,performance,portability,information,missingInclude",
-            "--inconclusive",
-            "-j 4",
-            "--template=gcc",
-            "$FILENAME",
-        },
-        to_stdin = true,
-        from_stderr = true,
-        -- choose an output format (raw, json, or line)
-        format = "line",
-        -- use helpers to parse the output from string matchers,
-        -- or parse it manually with a function
-        on_output = helpers.diagnostics.from_pattern(
-            [[(%d+):(%d+): (%w+): (.*)]], -- (%[(%w+)%])?
-            { "row", "col", "severity", "message" }, -- "code"
-            {
-                severities = {
-                    note = helpers.diagnostics.severities["information"],
-                },
-            }
-        ),
-    }),
-}
---info = h.diagnostics.severities["information"],
-null_ls.register({ name = "cppcheck", sources = { settings.cppcheck } })
-
 settings.clangd_setting = {
     cmd = {
         "clangd",
