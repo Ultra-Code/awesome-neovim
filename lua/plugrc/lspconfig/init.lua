a5972e6e08b26ac6ef8714b6938c72f9a705b33e
Ultra-Code <mega.alpha100@gmail.com>
2021-10-20 04:17:17 +0000

    Add more useful builtins for null-ls
    Add cppcheck for c++ static analysis
    Add shellcheck for sh/bash analysis
    Add gitsigns for code-actions of gitsigns

--- lua/plugrc/lspconfig/init.lua
+++ lua/plugrc/lspconfig/init.lua
@@ -126,6 +126,9 @@ local function setup_servers()
         null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.code_actions.gitsigns,
+        null_ls.builtins.diagnostics.shellcheck,
+        null_ls.builtins.diagnostics.cppcheck,
     }
 
     null_ls.config({
