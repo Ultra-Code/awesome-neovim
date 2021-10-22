a83e82760f06e07c28d66c83e1484e2b26c5a333
Ultra-Code <mega.alpha100@gmail.com>
2021-10-22 19:12:41 +0000

    Refactor tree setup into plugrc and leave only vim mappings in mapping

--- lua/plugrc/tree.lua
+++ lua/plugrc/tree.lua
@@ -1,6 +1,6 @@
 local g = vim.g
 
-g.nvim_tree_ignore = {'.git', 'node_modules', '.cache'} -- empty by default
+g.nvim_tree_ignore = { ".git", "node_modules", ".cache" } -- empty by default
 g.nvim_tree_gitignore = 1 -- 0 by default
 g.nvim_tree_quit_on_open = 1 -- 0 by default, closes the tree when you open a file
 --g.nvim_tree_follow = 1 -- 0 by default, this option allows the cursor to be updated when entering a buffer
@@ -16,55 +16,97 @@ g.nvim_tree_highlight_opened_files = 1 -- 0 by default, will enable folder and f
 g.nvim_tree_respect_buf_cwd = 1 --0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
 --g.nvim_tree_create_in_closed_folder = 0 --1 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
 --g.nvim_tree_refresh_wait = 500 --1000 by default, control how often the tree can be refreshed, 1000 means the tree can be refresh once per 1000ms.
-g.nvim_tree_window_picker_exclude = {
-    filetype = {'packer', 'qf'},
-    buftype = {'terminal'}
-}
--- Dictionary of buffer option names mapped to a list of option values that
--- indicates to the window picker that the buffer's window should not be
--- selectable.
---[[
-   [g.nvim_tree_special_files = [ 'README.md'] -- List of filenames that gets highlighted with NvimTreeSpecialFile
-   [g.nvim_tree_show_icons = {
-   [    \ 'git': 1,
-   [    \ 'folders': 1,
-   [    \ 'files': 1,
-   [    \ }
-   ]]
--- If 0, do not show the icons for one of 'git' 'folder' and 'files'
--- 1 by default, notice that if 'files' is 1, it will only display
--- if nvim-web-devicons is installed and on your runtimepath
 
--- default will show icon by default if no icon is provided
--- default shows no icon by default
---[[
-   [g.nvim_tree_icons = {
-   [    \ 'default': '',
-   [    \ 'symlink': '',
-   [    \ 'git': {
-   [    \   'unstaged': --✗--,
-   [    \   'staged': --✓--,
-   [    \   'unmerged': ----,
-   [    \   'renamed': --➜--,
-   [    \   'untracked': --★--,
-   [    \   'deleted': ----,
-   [    \   'ignored': --◌--
-   [    \   },
-   [    \ 'folder': {
-   [    \   'default': ----,
-   [    \   'open': ----,
-   [    \   'empty': ----,
-   [    \   'empty_open': ----,
-   [    \   'symlink': ----,
-   [    \   'symlink_open': ----,
-   [    \   },
-   [    \   'lsp': {
-   [    \     'hint': ----,
-   [    \     'info': ----,
-   [    \     'warning': ----,
-   [    \     'error': ----,
-   [    \   }
-   [    \ }
-   ]]
--- a list of groups can be found at `:help nvim_tree_highlight`
--- highlight NvimTreeFolderIcon guibg=blue
+local tree_cb = require("nvim-tree.config").nvim_tree_callback
+
+require("nvim-tree").setup({
+    -- disables netrw completely
+    disable_netrw = true,
+    -- hijack netrw window on startup
+    hijack_netrw = true,
+    -- open the tree when running this setup function
+    open_on_setup = false,
+    -- will not open on setup if the filetype is in this list
+    ignore_ft_on_setup = {},
+    -- closes neovim automatically when the tree is the last **WINDOW** in the view
+    auto_close = true,
+    -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+    open_on_tab = false,
+    -- hijacks new directory buffers when they are opened.
+    update_to_buf_dir = {
+        -- enable the feature
+        enable = true,
+        -- allow to open the tree if it was previously closed
+        auto_open = true,
+    },
+    -- hijack the cursor in the tree to put it at the start of the filename
+    hijack_cursor = false,
+    -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
+    update_cwd = true,
+    -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+    update_focused_file = {
+        -- enables the feature
+        enable = false,
+        -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+        -- only relevant when `update_focused_file.enable` is true
+        update_cwd = false,
+        -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+        -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+        ignore_list = {},
+    },
+    -- configuration options for the system open command (`s` in the tree by default)
+    system_open = {
+        -- the command to run this, leaving nil should work in most cases
+        cmd = nil,
+        -- the command arguments as a list
+        args = {},
+    },
+    view = {
+        -- width of the window, can be either a number (columns) or a string in `%`
+        width = 30,
+        -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
+        side = "left",
+        -- if true the tree will resize itself after opening a file
+        auto_resize = false,
+        mappings = {
+            -- custom only false will merge the list with the default mappings
+            -- if true, it will only use your list to set the mappings
+            custom_only = false,
+            -- list of mappings to set on the tree manually
+            list = {
+                { key = { "<CR>", "o", "<2-LeftMouse>" }, cb = tree_cb("edit") },
+                { key = { "<2-RightMouse>", "<C-]>" }, cb = tree_cb("cd") },
+                { key = "<C-v>", cb = tree_cb("vsplit") },
+                { key = "<C-x>", cb = tree_cb("split") },
+                { key = "<C-t>", cb = tree_cb("tabnew") },
+                { key = "<", cb = tree_cb("prev_sibling") },
+                { key = ">", cb = tree_cb("next_sibling") },
+                { key = "P", cb = tree_cb("parent_node") },
+                { key = "<BS>", cb = tree_cb("close_node") },
+                { key = "<S-CR>", cb = tree_cb("close_node") },
+                { key = "<Tab>", cb = tree_cb("preview") },
+                { key = "K", cb = tree_cb("first_sibling") },
+                { key = "J", cb = tree_cb("last_sibling") },
+                { key = "I", cb = tree_cb("toggle_ignored") },
+                { key = "H", cb = tree_cb("toggle_dotfiles") },
+                { key = "R", cb = tree_cb("refresh") },
+                { key = "a", cb = tree_cb("create") },
+                { key = "d", cb = tree_cb("remove") },
+                { key = "r", cb = tree_cb("rename") },
+                { key = "<C-r>", cb = tree_cb("full_rename") },
+                { key = "x", cb = tree_cb("cut") },
+                { key = "c", cb = tree_cb("copy") },
+                { key = "p", cb = tree_cb("paste") },
+                { key = "y", cb = tree_cb("copy_name") },
+                { key = "Y", cb = tree_cb("copy_path") },
+                { key = "gy", cb = tree_cb("copy_absolute_path") },
+                { key = "[c", cb = tree_cb("prev_git_item") },
+                { key = "]c", cb = tree_cb("next_git_item") },
+                { key = "-", cb = tree_cb("dir_up") },
+                { key = "s", cb = tree_cb("system_open") },
+                { key = "q", cb = tree_cb("close") },
+                { key = "g?", cb = tree_cb("toggle_help") },
+            },
+        },
+    },
+})
