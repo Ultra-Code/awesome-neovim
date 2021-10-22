24cfb6f9bdbff0324322d325d3655e184f5b7d7c
Ultra-Code <mega.alpha100@gmail.com>
2021-10-22 18:57:29 +0000

    Remove eslint and prettier from null-ls sources

--- lua/plugrc/lspconfig/init.lua
+++ lua/plugrc/lspconfig/init.lua
@@ -122,9 +122,6 @@ local function setup_servers()
 
     local null_ls = require("null-ls")
     local sources = {
-        null_ls.builtins.formatting.eslint,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.code_actions.gitsigns,
         null_ls.builtins.diagnostics.shellcheck,
