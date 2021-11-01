bf5e0929dd5263c9729a2c2e573f4180677f9d32
Ultra-Code <mega.alpha100@gmail.com>
2021-11-01 23:46:48 +0000

    Remove unused global options and add filters option to setup
    Enable open_on_setup and update_focused_file

--- lua/plugrc/tree.lua
+++ lua/plugrc/tree.lua
@@ -1,21 +1,9 @@
 local g = vim.g
 
-g.nvim_tree_ignore = { ".git", "node_modules", ".cache" } -- empty by default
 g.nvim_tree_gitignore = 1 -- 0 by default
 g.nvim_tree_quit_on_open = 1 -- 0 by default, closes the tree when you open a file
---g.nvim_tree_follow = 1 -- 0 by default, this option allows the cursor to be updated when entering a buffer
-g.nvim_tree_indent_markers = 0 -- 0 by default, this option shows indent markers when folders are open
--- g.nvim_tree_hide_dotfiles = 1 --0 by default, this option hides files and folders starting with a dot `.`
-g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
 g.nvim_tree_highlight_opened_files = 1 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
---g.nvim_tree_root_folder_modifier = ':~' --This is the default. See :help filename-modifiers for more options
---g.nvim_tree_add_trailing = 1 --0 by default, append a trailing slash to folder names
---g.nvim_tree_group_empty = 1 -- 0 by default, compact folders that only contain a single folder into one node in the file tree
 --g.nvim_tree_disable_window_picker = 1 --0 by default, will disable the window picker.
---g.nvim_tree_symlink_arrow = ' >> ' " defaults to ' ➛ '. used as a separator between symlinks' source and target.
-g.nvim_tree_respect_buf_cwd = 1 --0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
---g.nvim_tree_create_in_closed_folder = 0 --1 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
---g.nvim_tree_refresh_wait = 500 --1000 by default, control how often the tree can be refreshed, 1000 means the tree can be refresh once per 1000ms.
 
 local tree_cb = require("nvim-tree.config").nvim_tree_callback
 
@@ -24,13 +12,14 @@ require("nvim-tree").setup({
     disable_netrw = true,
     -- hijack netrw window on startup
     hijack_netrw = true,
-    -- open the tree when running this setup function
-    open_on_setup = false,
+    -- open tree when running this setup function and the
+    -- current buffer is a directory, is empty or is unnamed.
+    open_on_setup = true,
     -- will not open on setup if the filetype is in this list
-    ignore_ft_on_setup = {},
+    ignore_ft_on_setup = { ".git", "node_modules", ".cache" },
     -- closes neovim automatically when the tree is the last **WINDOW** in the view
     auto_close = true,
-    -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+    -- opens the tree when changing/opening a new tab if the tree was previously opened
     open_on_tab = false,
     -- hijacks new directory buffers when they are opened.
     update_to_buf_dir = {
@@ -46,10 +35,10 @@ require("nvim-tree").setup({
     -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
     update_focused_file = {
         -- enables the feature
-        enable = false,
+        enable = true,
         -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
         -- only relevant when `update_focused_file.enable` is true
-        update_cwd = false,
+        update_cwd = true,
         -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
         -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
         ignore_list = {},
@@ -63,7 +52,7 @@ require("nvim-tree").setup({
     },
     view = {
         -- width of the window, can be either a number (columns) or a string in `%`
-        width = 30,
+        width = 39,
         -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
         side = "left",
         -- if true the tree will resize itself after opening a file
@@ -74,7 +63,10 @@ require("nvim-tree").setup({
             custom_only = false,
             -- list of mappings to set on the tree manually
             list = {
-                { key = { "<CR>", "o", "<2-LeftMouse>" }, cb = tree_cb("edit") },
+                {
+                    key = { "<CR>", "o", "<2-LeftMouse>" },
+                    cb = tree_cb("edit"),
+                },
                 { key = { "<2-RightMouse>", "<C-]>" }, cb = tree_cb("cd") },
                 { key = "<C-v>", cb = tree_cb("vsplit") },
                 { key = "<C-x>", cb = tree_cb("split") },
@@ -108,5 +100,9 @@ require("nvim-tree").setup({
                 { key = "g?", cb = tree_cb("toggle_help") },
             },
         },
+    },
+    filters = {
+        dotfiles = false,
+        custom = {},
     },
 })
