cfee7bc8c4fac75b2baedc83821f3a5359146a77
Ultra-Code <mega.alpha100@gmail.com>
2021-11-22 20:10:39 +0000

    Remove null_ls codeaction for gitsigns

--- lua/plugrc/lspconfig/init.lua
+++ lua/plugrc/lspconfig/init.lua
@@ -78,7 +78,6 @@ local function setup_servers()
     local null_ls = require("null-ls")
     local sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.code_actions.gitsigns,
         null_ls.builtins.diagnostics.shellcheck,
         null_ls.builtins.diagnostics.cppcheck.with({
             extra_args = {
