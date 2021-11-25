ec5f12b8d23f8e4e891a3df649be7ba9afac1de5
Ultra-Code <mega.alpha100@gmail.com>
2021-11-25 22:20:57 +0000

    Update ui to use onedark theme

--- lua/ui.lua
+++ lua/ui.lua
@@ -2,18 +2,18 @@
 vim.o.termguicolors = true
 
 -- Onedark colorscheme
---vim.g.onedark_style = 'deep' --darker,default,cool ,deep,warm,warmer
+vim.g.onedark_style = "deep" --darker,default,cool ,deep,warm,warmer
 
---require('onedark').setup()
+require("onedark").setup()
 -- For dark theme
-vim.g.vscode_style = "dark"
-
-vim.cmd[[colorscheme vscode]]
+--vim.g.vscode_style = "dark"
+--
+--vim.cmd[[colorscheme vscode]]
 -- status line
-require('plugrc/lualine')
+require("plugrc/lualine")
 
 -- buffer line
 require("plugrc/bufferline")
 
 -- Treesitter syntax highlighting
-require('plugrc/treesitter')
+require("plugrc/treesitter")
