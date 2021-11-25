64a9afc5250581499f297e0034ec1821f603ad80
Ultra-Code <mega.alpha100@gmail.com>
2021-11-25 06:11:10 +0000

    Remove ftdetect and ftplugin since I don't need it anymore

--- lua/ftplugin/vimwiki.lua
+++
@@ -1,5 +0,0 @@
--- Check docs :h vimwiki-list
-vim.cmd[[
-autocmd FileType vimwiki inoremap <silent><buffer> <CR>  <C-]><Esc>:VimwikiReturn 3 3<CR>
-autocmd FileType vimwiki inoremap <silent><buffer> <S-CR>  <Esc>:VimwikiReturn 2 2<CR>
-]]
