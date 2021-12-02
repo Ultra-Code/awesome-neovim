f9f9eeb4d9fd86556e7a5c926494c0941fe47836
Ultra-Code <mega.alpha100@gmail.com>
2021-12-02 17:26:10 +0000

    Rename lspconfig.config to lspconfig.ui_customizations
    Remove redundant functionalities

--- lua/plugrc/lspconfig/ui_customizations.lua
+++ lua/plugrc/lspconfig/ui_customizations.lua
@@ -41,21 +41,9 @@ function M.sign_column_diagnostic_symbols()
         Information = " ",
     }
     for type, icon in pairs(signs) do
-        local hl = "LspDiagnosticsSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+        local hl = "DiagnosticsSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
     end
-end
-
-function M.customise_diagnostics()
-    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-        vim.lsp.diagnostic.on_publish_diagnostics,
-        {
-            virtual_text = false,
-            update_in_insert = true,
-            signs = true,
-            underline = true,
-        }
-    )
 end
 
 return M
