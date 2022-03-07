5d70c10429ec1e69d47505ba5792f789fd49e00d
Ultra-Code <mega.alpha100@gmail.com>
2022-03-07 05:37:05 +0000

    Update nvim-tree config

--- lua/plugrc/tree.lua
+++ lua/plugrc/tree.lua
@@ -1,10 +1,6 @@
 local g = vim.g
 
-g.nvim_tree_quit_on_open = 1 -- 0 by default, closes the tree when you open a file
 g.nvim_tree_highlight_opened_files = 1 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
---g.nvim_tree_disable_window_picker = 1 --0 by default, will disable the window picker.
-
-local tree_cb = require("nvim-tree.config").nvim_tree_callback
 
 require("nvim-tree").setup({
     -- disables netrw completely
@@ -14,17 +10,26 @@ require("nvim-tree").setup({
     -- open tree when running this setup function and the
     -- current buffer is a directory, is empty or is unnamed.
     open_on_setup = true,
+    -- will ignore the buffer, when deciding to open the tree on setup
+    ignore_buffer_on_setup = false,
     -- will not open on setup if the filetype is in this list
     ignore_ft_on_setup = { ".git", "node_modules", ".cache" },
     -- closes neovim automatically when the tree is the last **WINDOW** in the view
     auto_close = true,
+    --  reload the explorer every time a buffer is written to
+    auto_reload_on_write = true,
     -- opens the tree when changing/opening a new tab if the tree was previously opened
     open_on_tab = false,
-    -- hijacks new directory buffers when they are opened.
-    update_to_buf_dir = {
-        -- enable the feature
+    -- changes how files within the same directory are sorted. can be name | modification_time
+    --    sort_by = "name",
+    --  open in place of the unnamed buffer if it's empty.
+    hijack_unnamed_buffer_when_opening = false,
+    -- hijacks new directory buffers when they are opened (`:e dir`).
+    hijack_directories = {
+        -- enable the feature. Disable this option if you use vim-dirvish or dirbuf.nvim.
+        -- If |hijack_netrw| and |disable_netrw| are `false`, this feature will be disabled.
         enable = true,
-        -- allow to open the tree if it was previously closed
+        -- opens the tree if the tree was previously closed.
         auto_open = true,
     },
     -- hijack the cursor in the tree to put it at the start of the filename
@@ -49,15 +54,30 @@ require("nvim-tree").setup({
         -- the command arguments as a list
         args = {},
     },
+    -- git integration with icons and colors
     git = {
+        -- enable / disable the feature
         enable = true,
+        -- ignore files based on `.gitignore`
         ignore = true,
     },
+    -- window / buffer setup
     view = {
-        -- width of the window, can be either a number (columns) or a string in `%`
+        -- hide the path of the current working directory on top of the tree
+        hide_root_folder = false,
+        --  width of the window, can be either a `%` string or a number representing columns.
+        --  Only works with |view.side| `left` or `right`
         width = 39,
         -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
         side = "left",
+        -- preserve window proportions when opening a file.
+        -- If `false`, the height and width of windows other than nvim-tree will be equalized.
+        preserve_window_proportions = true,
+        -- print the line number in front of each line.
+        number = false,
+        -- show the line number relative to the line with the cursor in front of each line.
+        -- If the option `view.number` is also `true`, the number on the cursor line will be the line number instead of `0`.
+        relativenumber = false,
         -- if true the tree will resize itself after opening a file
         auto_resize = false,
         mappings = {
@@ -68,45 +88,112 @@ require("nvim-tree").setup({
             list = {
                 {
                     key = { "<CR>", "o", "<2-LeftMouse>" },
-                    cb = tree_cb("edit"),
+                    action = "edit",
                 },
-                { key = { "<2-RightMouse>", "<C-]>" }, cb = tree_cb("cd") },
-                { key = "<C-v>", cb = tree_cb("vsplit") },
-                { key = "<C-x>", cb = tree_cb("split") },
-                { key = "<C-t>", cb = tree_cb("tabnew") },
-                { key = "<", cb = tree_cb("prev_sibling") },
-                { key = ">", cb = tree_cb("next_sibling") },
-                { key = "P", cb = tree_cb("parent_node") },
-                { key = "<BS>", cb = tree_cb("close_node") },
-                { key = "<S-CR>", cb = tree_cb("close_node") },
-                { key = "<Tab>", cb = tree_cb("preview") },
-                { key = "K", cb = tree_cb("first_sibling") },
-                { key = "J", cb = tree_cb("last_sibling") },
-                { key = "I", cb = tree_cb("toggle_ignored") },
-                { key = "H", cb = tree_cb("toggle_dotfiles") },
-                { key = "R", cb = tree_cb("refresh") },
-                { key = "a", cb = tree_cb("create") },
-                { key = "d", cb = tree_cb("remove") },
-                { key = "r", cb = tree_cb("rename") },
-                { key = "<C-r>", cb = tree_cb("full_rename") },
-                { key = "x", cb = tree_cb("cut") },
-                { key = "c", cb = tree_cb("copy") },
-                { key = "p", cb = tree_cb("paste") },
-                { key = "y", cb = tree_cb("copy_name") },
-                { key = "Y", cb = tree_cb("copy_path") },
-                { key = "gy", cb = tree_cb("copy_absolute_path") },
-                { key = "[c", cb = tree_cb("prev_git_item") },
-                { key = "]c", cb = tree_cb("next_git_item") },
-                { key = "-", cb = tree_cb("dir_up") },
-                { key = "s", cb = tree_cb("system_open") },
-                { key = "q", cb = tree_cb("close") },
-                { key = "g?", cb = tree_cb("toggle_help") },
+                {
+                    key = "<C-e>",
+                    action = "edit_in_place",
+                },
+                {
+                    key = { "O" },
+                    action = "edit_no_picker",
+                },
+                { key = "<2-RightMouse>", action = "cd" },
+                { key = "<C-v>", action = "vsplit" },
+                { key = "<C-x>", action = "split" },
+                { key = "<C-t>", action = "tabnew" },
+                { key = "<", action = "prev_sibling" },
+                { key = ">", action = "next_sibling" },
+                { key = "P", action = "parent_node" },
+                { key = "<BS>", action = "close_node" },
+                { key = "<Tab>", action = "preview" },
+                { key = "K", action = "first_sibling" },
+                { key = "J", action = "last_sibling" },
+                { key = "I", action = "toggle_ignored" },
+                { key = "H", action = "toggle_dotfiles" },
+                { key = "R", action = "refresh" },
+                { key = "a", action = "create" },
+                { key = "d", action = "remove" },
+                { key = "r", action = "rename" },
+                { key = "<C-r>", action = "full_rename" },
+                { key = "x", action = "cut" },
+                { key = "c", action = "copy" },
+                { key = "p", action = "paste" },
+                { key = "y", action = "copy_name" },
+                { key = "Y", action = "copy_path" },
+                { key = "gy", action = "copy_absolute_path" },
+                { key = "[c", action = "prev_git_item" },
+                { key = "]c", action = "next_git_item" },
+                { key = "-", action = "dir_up" },
+                { key = "s", action = "system_open" },
+                { key = "q", action = "close" },
+                { key = "g?", action = "toggle_help" },
+                {
+                    key = "W",
+                    action = "collapse_all",
+                },
+                {
+                    key = "S",
+                    action = "search_node",
+                },
+                {
+                    key = "<C-k>",
+                    action = "toggle_file_info",
+                },
+                {
+                    key = ".",
+                    action = "run_file_command",
+                },
             },
         },
-    },
-    filters = {
-        --hide_dotfiles
-        dotfiles = false,
-        custom = {},
+        filters = {
+            -- show `dotfiles` (files starting with a `.`)
+            dotfiles = false,
+            -- custom list of string that will not be shown.
+            custom = {},
+            -- list of directories or files to exclude from filtering
+            exclude = {},
+        },
+        -- hijacks new directory buffers when they are opened.
+        update_to_buf_dir = {
+            -- enable the feature
+            enable = true,
+            -- allow to open the tree if it was previously closed
+            auto_open = true,
+        },
+        -- configuration for various actions
+        actions = {
+            change_dir = {
+                -- change the working directory when changing directories in the tree.
+                enable = true,
+                -- use `:cd` instead of `:lcd` when changing directories. Consider that this might cause issues with the update_cwd options.
+                global = false,
+            },
+            open_file = {
+                -- closes the explorer when opening a file. It will also disable preventing a buffer overriding the tree.
+                quit_on_open = true,
+                -- resizes the tree when opening a file
+                resize_window = false,
+                -- window picker configuration
+                window_picker = {
+                    -- Enable the feature. If the feature is not enabled, files will open in window from which you last opened the tree.
+                    enable = true,
+                    -- A string of chars used as identifiers by the window picker.
+                    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+                    -- Table of buffer option names mapped to a list of option values that indicates to the picker that the buffer's window should not be selectable.
+                    exclude = {
+                        filetype = {
+                            "notify",
+                            "packer",
+                            "qf",
+                            "diff",
+                            "fugitive",
+                            "fugitiveblame",
+                        },
+                        buftype = { "nofile", "terminal", "help" },
+                    },
+                },
+            },
+        },
     },
 })
