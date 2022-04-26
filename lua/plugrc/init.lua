e9eae1617c833be8bf9073ed72e4b776f030713e
Ultra-Code <mega.alpha100@gmail.com>
2022-04-26 01:00:21 +0000

    Use upstreamed norg treesitter config
    Remove unneeded snippet for configuring neorg for treesitter since norg treesitter
    configurations have been upstreamed to nvim-treesitter
    Require all files in plugrc in init

--- lua/plugrc/init.lua
+++ lua/plugrc/init.lua
@@ -2,12 +2,15 @@
 --==             PLUGINS INIT                             ==
 --==========================================================
 --
-require("plugrc.lspconfig")
+require("plugrc.bufferline")
+require("plugrc.lualine")
 require("plugrc.gitsigns")
+require("plugrc.lspconfig")
 require("plugrc.compe")
 require("plugrc.autopairs")
+require("plugrc.treesitter")
+require("plugrc.neorg")
 require("plugrc.whichkey")
 require("plugrc.tree")
-require("plugrc.neorg")
-require("Comment").setup()
 require("plugrc.trouble")
+require("Comment").setup()
--- lua/plugrc/treesitter.lua
+++ lua/plugrc/treesitter.lua
@@ -1,29 +1,3 @@
-local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
-
-parser_configs.norg_meta = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
-        files = { "src/parser.c" },
-        branch = "main",
-    },
-}
-
-parser_configs.norg = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg",
-        files = { "src/parser.c", "src/scanner.cc" },
-        branch = "main",
-    },
-}
-
-parser_configs.norg_table = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
-        files = { "src/parser.c" },
-        branch = "main",
-    },
-}
-
 require("nvim-treesitter.configs").setup({
     ensure_installed = {
         "cpp",
@@ -37,8 +11,6 @@ require("nvim-treesitter.configs").setup({
         "scss",
         "html",
         "norg",
-        "norg_table",
-        "norg_meta",
         "glsl",
         "vim",
     },
