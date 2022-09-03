1e5d42ae6a2cb1c44aaf29fc82754dbfb2d4b0a5
Ultra-Code <mega.alpha100@gmail.com>
2022-09-03 16:22:45 +0000

    (lazy:symbols) Update config

--- lua/lazy/symbols.lua
+++ lua/lazy/symbols.lua
@@ -1,57 +1,48 @@
-vim.g.symbols_outline = {
-    highlight_hovered_item = true,
-    show_guides = true,
-    auto_preview = true,
-    position = "right",
-    width = 25,
-    show_numbers = false,
-    show_relative_numbers = false,
-    show_symbol_details = true,
-    preview_bg_highlight = "Pmenu",
-    keymaps = { -- These keymaps can be a string or a table for multiple keys
-        close = { "<Esc>", "q" },
-        goto_location = "<Cr>",
-        focus_location = "o",
-        hover_symbol = "<C-space>",
-        toggle_preview = "K",
-        rename_symbol = "r",
-        code_actions = "a",
-    },
-    lsp_blacklist = {},
-    symbol_blacklist = {},
-    symbols = {
-        File = { icon = "", hl = "TSURI" },
-        Module = { icon = "", hl = "TSNamespace" },
-        Namespace = { icon = "", hl = "TSNamespace" },
-        Package = { icon = "", hl = "TSNamespace" },
-        Class = { icon = "𝓒", hl = "TSType" },
-        Method = { icon = "ƒ", hl = "TSMethod" },
-        Property = { icon = "", hl = "TSMethod" },
-        Field = { icon = "", hl = "TSField" },
-        Constructor = { icon = "", hl = "TSConstructor" },
-        Enum = { icon = "ℰ", hl = "TSType" },
-        Interface = { icon = "ﰮ", hl = "TSType" },
-        Function = { icon = "", hl = "TSFunction" },
-        Variable = { icon = "", hl = "TSConstant" },
-        Constant = { icon = "", hl = "TSConstant" },
-        String = { icon = "𝓐", hl = "TSString" },
-        Number = { icon = "#", hl = "TSNumber" },
-        Boolean = { icon = "⊨", hl = "TSBoolean" },
-        Array = { icon = "", hl = "TSConstant" },
-        Object = { icon = "⦿", hl = "TSType" },
-        Key = { icon = "🔐", hl = "TSType" },
-        Null = { icon = "NULL", hl = "TSType" },
-        EnumMember = { icon = "", hl = "TSField" },
-        Struct = { icon = "𝓢", hl = "TSType" },
-        Event = { icon = "🗲", hl = "TSType" },
-        Operator = { icon = "+", hl = "TSOperator" },
-        TypeParameter = { icon = "𝙏", hl = "TSParameter" },
-    },
-}
 require("utils")
+
+local function symbols_setup()
+    vim.cmd([[ packadd symbols-outline.nvim ]])
+
+    local symbols_outline_opts = {
+        highlight_hovered_item = true,
+        show_guides = true,
+        auto_preview = false,
+        position = 'right',
+        relative_width = true,
+        width = 25,
+        auto_close = false,
+        show_numbers = false,
+        show_relative_numbers = false,
+        show_symbol_details = true,
+        preview_bg_highlight = 'Pmenu',
+        autofold_depth = nil,
+        auto_unfold_hover = true,
+        fold_markers = { '', '' },
+        wrap = false,
+        keymaps = { -- These keymaps can be a string or a table for multiple keys
+            close = { "jk", "q" },
+            goto_location = "<Cr>",
+            focus_location = "o",
+            hover_symbol = "<C-space>",
+            toggle_preview = "p",
+            rename_symbol = "r",
+            code_actions = "a",
+            fold = "zf",
+            unfold = "zu",
+            fold_all = "zC",
+            unfold_all = "zO",
+            fold_reset = "zR",
+        },
+    }
+    require("symbols-outline").setup(symbols_outline_opts)
+end
+
 map(
     "n",
     "<leader>so",
-    "<cmd>packadd symbols-outline.nvim<cr>|<cmd>SymbolsOutline<cr>",
+    function()
+        symbols_setup()
+        vim.cmd([[SymbolsOutline]])
+    end,
     { silent = true }
 )
