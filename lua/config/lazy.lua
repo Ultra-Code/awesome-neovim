6596aff82b1adb8267ce74ea21d5b4d53bcb58a4
Ultra-Code <mega.alpha100@gmail.com>
2023-03-24 17:03:12 +0000

    lazy.nvim: disable notification for changes detected

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,11 @@ require("lazy").setup({
         -- version = "*", -- try installing the latest stable version for plugins that support semver
     },
     checker = { enabled = false }, -- automatically check for plugin updates
+    change_detection = {
+        -- automatically check for config file changes and reload the ui
+        enabled = true,
+        notify = false, -- get a notification when changes are found
+    },
     performance = {
         cache = {
             enabled = true,
