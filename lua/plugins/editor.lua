08e00774f8bc4b99c227af65e7066b79df6216a3
Ultra-Code <mega.alpha100@gmail.com>
2023-04-10 16:48:00 +0000

    editor(hop): disable nvim_echo in hop.nvim
    The prompts are annoying and disrupts workflow when the text is at the
    end of the screen

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -55,11 +55,16 @@ return {
             }
         },
         config = function()
-            -- place this in one of your configuration file(s)
             local mode = { "n", "x", "o" }
             local hop = require('hop')
             local directions = require('hop.hint').HintDirection
+
+            local echo = vim.api.nvim_echo
+            local noop = function()
+            end
             vim.keymap.set(mode, 'f', function()
+                --disable nvim_echo temporarily
+                vim.api.nvim_echo = noop
                 hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
             end, { remap = true })
             vim.keymap.set(mode, 'F', function()
@@ -71,7 +76,12 @@ return {
             vim.keymap.set(mode, 'T', function()
                 hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
             end, { remap = true })
+
             require("hop").setup()
+
+            if vim.api.nvim_echo == noop then
+                vim.api.nvim_echo = echo
+            end
         end,
     },
     -- library used by other plugins
