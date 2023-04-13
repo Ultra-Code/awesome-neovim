1aea43432d97ca2c2d29f94b9e24afc1776b17ec
Ultra-Code <mega.alpha100@gmail.com>
2023-04-13 14:48:07 +0000

    autocmd:execute restore cursor in a oneline file also

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -43,7 +43,7 @@ autocmd("BufReadPost", {
     callback = function()
         local mark = vim.api.nvim_buf_get_mark(0, '"')
         local lcount = vim.api.nvim_buf_line_count(0)
-        if mark[1] > 1 and mark[1] <= lcount then
+        if mark[1] >= 1 and mark[1] <= lcount then
             pcall(vim.api.nvim_win_set_cursor, 0, mark)
         end
     end,
