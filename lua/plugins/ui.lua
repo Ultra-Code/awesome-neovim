267747b32e0f1a0b0a72b600d7ed0020aa0d0aa0
Ultra-Code <mega.alpha100@gmail.com>
2023-04-15 16:17:10 +0000

    ui: move treesitter and colorscheme to different plugin files

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,49 +1,4 @@
 return {
-
-    -- Onedark colorscheme
-    {
-        "navarasu/onedark.nvim",
-        event = "BufEnter",
-        config = function(_, opts)
-            require("onedark").setup(opts)
-            require("onedark").load()
-        end,
-        opts = {
-            -- Main options --
-            style = 'deep',               -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-            transparent = false,          -- Show/hide background
-            term_colors = true,           -- Change terminal color as per the selected theme style
-            ending_tildes = false,        -- Show the end-of-buffer tildes. By default they are hidden
-            cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
-            -- toggle theme style ---
-            toggle_style_key = nil,       -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-            -- List of styles to toggle between
-            toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' },
-            -- Change code style ---
-            -- Options are italic, bold, underline, none
-            -- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
-            code_style = {
-                comments = 'italic',
-                keywords = 'none',
-                functions = 'none',
-                strings = 'none',
-                variables = 'none'
-            },
-            -- Lualine options --
-            lualine = {
-                transparent = false, -- lualine center bar transparency
-            },
-            -- Custom Highlights --
-            colors = {},     -- Override default colors
-            highlights = {}, -- Override highlight groups
-            -- Plugins Config --
-            diagnostics = {
-                darker = true,     -- darker colors for diagnostic
-                undercurl = true,  -- use undercurl instead of underline for diagnostics
-                background = true, -- use background color for virtual text
-            },
-        }
-    },
     -- better vim.ui
     { "MunifTanjim/nui.nvim", lazy = true },
     -- Better `vim.notify()`
@@ -151,7 +106,7 @@ return {
                 hover = {
                     enabled = true,
                     delay = 200,
-                    reveal = { 'close' }
+                    reveal = { "close" }
                 },
                 show_buffer_icons = true,       --| false, -- disable filetype icons for buffers
                 show_buffer_close_icons = true, --| false,
@@ -186,7 +141,7 @@ return {
                             priority = 2,                                  -- determines where it will appear relative to other groups (Optional)
                             icon = "",                                  -- Optional
                             matcher = function(buf)                        -- Mandatory
-                                return buf.filename:match('%_test') or buf.filename:match('%_spec')
+                                return buf.filename:match("%_test") or buf.filename:match("%_spec")
                             end,
                         },
                         {
@@ -194,7 +149,7 @@ return {
                             highlight = { undercurl = true, sp = "green" },
                             auto_close = false, -- whether or not close this group if it doesn't contain the current buffer
                             matcher = function(buf)
-                                return buf.filename:match('%.md') or buf.filename:match('%.txt')
+                                return buf.filename:match("%.md") or buf.filename:match("%.txt")
                             end,
                             separator = { -- Optional
                                 -- style = require('bufferline.groups').separator.tab
@@ -217,7 +172,7 @@ return {
 
             return {
                 options = {
-                    theme = "auto",
+                    theme = "catppuccin", -- "onedark" "auto",
                     component_separators = "|",
                     section_separators = { left = "", right = "" },
                 },
@@ -231,59 +186,13 @@ return {
                         file_status = true, -- displays file status (readonly status, modified status)
                         path = 1,           -- 0 = just filename, 1 = relative path, 2 = absolute path
                     } },
-                    lualine_x = { 'encoding', 'fileformat' },
+                    lualine_x = { "encoding", "fileformat" },
                     lualine_y = { "filetype", total_num_lines },
                     lualine_z = {
                         { "location", separator = { right = "" }, left_padding = 2 },
                     },
                 },
             }
-        end,
-    },
-    -- === Syntax Plugins ===
-    -- Nvim Treesitter configurations and abstraction layer
-    {
-        "nvim-treesitter/nvim-treesitter",
-        event = { "BufReadPost", "BufNewFile" },
-        keys = {
-            { "<c-space>", desc = "Increment selection" },
-            { "<bs>",      desc = "Decrement selection", mode = "x" },
-        },
-        build = function()
-            vim.cmd("TSUpdate")
-        end,
-        opts = {
-            -- A list of parser names, or "all" (the 1st five listed parsers should always be installed)
-            -- For web dev "jsonc", "typescript", "css", "html", "vue",
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "cpp", "zig", "norg", "bash", "glsl" },
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
-            ignore_install = {}, -- List of parsers to ignore installing
-            highlight = {
-                enable = true,   -- false will disable the whole extension
-                disable = {},    -- list of language that will be disabled
-            },
-            --Incremental selection based on the named nodes from the grammar.
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<C-space>",
-                    node_incremental = "<C-space>",
-                    scope_incremental = "<nop>",
-                    node_decremental = "<bs>",
-                },
-            },
-            --Indentation based on treesitter for the = operator.
-            indent = {
-                enable = true,
-            },
-            autopairs = { enable = true },
-        },
-        config = function(_, opts)
-            require("nvim-treesitter.configs").setup(opts)
         end,
     },
 }
