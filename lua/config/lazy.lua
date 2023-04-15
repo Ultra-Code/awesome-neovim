46def12ce4d1081275278b9861347c31733ab343
Ultra-Code <mega.alpha100@gmail.com>
2023-04-15 13:30:56 +0000

    lazy: enable use of some useful rtp plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,21 +28,21 @@ require("lazy").setup({
         rtp = {
             -- disable some rtp plugins
             disabled_plugins = {
-                "gzip",
-                "tarPlugin",
-                "tohtml",
-                "tutor",
-                "zipPlugin",
-                "matchit",
-                "netrwPlugin",
-                "rplugin",
-                "matchparen",
-                "man",
-                "shada",
-                "spellfile",
-                "health",
-                "editorconfig",
-                "nvim"
+                "gzip",        -- Vim plugin for editing compressed files.
+                "tarPlugin",   -- tarPlugin.vim -- a Vim plugin for browsing tarfiles
+                "tohtml",      -- Vim plugin for converting a syntax highlighted file to HTML.
+                "zipPlugin",   -- zipPlugin.vim: Handles browsing zipfiles
+                "netrwPlugin", -- netrwPlugin.vim: Handles file transfer and remote directory listing across a network
+                "rplugin",     -- support of plugins written in other languages
+                -- "spellfile",   -- Vim plugin for downloading spell files
+                -- "matchit",  -- matchit.vim: (global plugin) Extended "%" matching
+                -- "matchparen", -- Vim plugin for showing matching parens
+                -- "tutor",
+                -- "man",
+                -- "shada",
+                -- "health",
+                -- "editorconfig",
+                -- "nvim",
             },
         },
     },
