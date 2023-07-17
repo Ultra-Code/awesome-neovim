4bd083ea4bf21371135b8f40c5d4a7b295210ab7
Ultra-Code <mega.alpha100@gmail.com>
2023-07-17 17:49:15 +0000

    ui: add indent guides

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,4 +1,58 @@
 return {
+    -- indent guides for Neovim
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        event = { "BufReadPost", "BufNewFile" },
+        opts = {
+            -- char = "▏",
+            char = "│",
+            filetype_exclude = {
+                "help",
+                "alpha",
+                "dashboard",
+                "neo-tree",
+                "Trouble",
+                "lazy",
+                "mason",
+                "notify",
+                "toggleterm",
+                "lazyterm",
+            },
+            show_trailing_blankline_indent = false,
+            show_current_context = false,
+        },
+    },
+
+    -- active indent guide and indent text objects
+    {
+        "echasnovski/mini.indentscope",
+        version = false, -- wait till new 0.7.0 release to put it back on semver
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {
+            -- symbol = "▏",
+            symbol = "│",
+            options = { try_as_border = true },
+        },
+        init = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = {
+                    "help",
+                    "alpha",
+                    "dashboard",
+                    "neo-tree",
+                    "Trouble",
+                    "lazy",
+                    "mason",
+                    "notify",
+                    "toggleterm",
+                    "lazyterm",
+                },
+                callback = function()
+                    vim.b.miniindentscope_disable = true
+                end,
+            })
+        end,
+    },
     -- better vim.ui
     { "MunifTanjim/nui.nvim", lazy = true },
     -- Better `vim.notify()`
