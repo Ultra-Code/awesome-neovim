1e92347c0c64bb5bf4d503b243ca0f783efc554b
Ultra-Code <mega.alpha100@gmail.com>
2023-07-18 16:49:49 +0000

    ui: use indentmini for indent guides

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,12 +1,11 @@
 return {
     -- indent guides for Neovim
     {
-        "lukas-reineke/indent-blankline.nvim",
-        event = { "BufReadPost", "BufNewFile" },
+        "nvimdev/indentmini.nvim",
+        event = { "BufEnter" },
         opts = {
-            -- char = "▏",
             char = "│",
-            filetype_exclude = {
+            exclude = {
                 "help",
                 "alpha",
                 "dashboard",
@@ -18,39 +17,10 @@ return {
                 "toggleterm",
                 "lazyterm",
             },
-            show_trailing_blankline_indent = false,
-            show_current_context = false,
         },
-    },
-
-    -- active indent guide and indent text objects
-    {
-        "echasnovski/mini.indentscope",
-        version = false, -- wait till new 0.7.0 release to put it back on semver
-        event = { "BufReadPre", "BufNewFile" },
-        opts = {
-            -- symbol = "▏",
-            symbol = "│",
-            options = { try_as_border = true },
-        },
-        init = function()
-            vim.api.nvim_create_autocmd("FileType", {
-                pattern = {
-                    "help",
-                    "alpha",
-                    "dashboard",
-                    "neo-tree",
-                    "Trouble",
-                    "lazy",
-                    "mason",
-                    "notify",
-                    "toggleterm",
-                    "lazyterm",
-                },
-                callback = function()
-                    vim.b.miniindentscope_disable = true
-                end,
-            })
+        config = function(_, opts)
+            require("indentmini").setup(opts)
+            vim.cmd.highlight("default link IndentLine LineNr")
         end,
     },
     -- better vim.ui
